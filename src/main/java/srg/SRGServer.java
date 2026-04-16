88532222a648547b12a661489259edf951ddcfc7
metanoeitaii <dhmhtrhspapandreou@gmail.com>
2026-04-16 15:33:15 +0300

    Updated SRGServer and search logic

--- src/main/java/srg/SRGServer.java
+++ src/main/java/srg/SRGServer.java
@@ -1,25 +1,31 @@
-package srg;
+    package srg;
 
-import java.net.*; //gia socket 
-import java.io.*;
+    import java.net.*; //gia socket 
+    import java.io.*;
 
-public class SRGServer {
+    public class SRGServer {
 
-    public static void main(String[] args) {
-        int port = 5000;
+        public static void main(String[] args) {
+           if (args.length < 1) {
+            System.out.println("Sfalma: Parakalw dwste to port tou SRG Server ws orisma!");
+            System.out.println("Paradeigma: java srg.SRGServer 5000");
+            return; // Σταματάει το πρόγραμμα εδώ αν δεν δώσεις port
+        }
+        
+        // Παίρνει το port αυστηρά από το τερματικό
+        int port = Integer.parseInt(args[0]);
+            try (ServerSocket serverSocket = new ServerSocket(port)) {//gia na perimenei na syndethei o worker
+                System.out.println("SRG Server started on port " + port);
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {//gia na perimenei na syndethei o worker
-            System.out.println("SRG Server started on port " + port);
+                while (true) {
+                    Socket socket = serverSocket.accept();//otan syndetai enas worker, to accept epistrefei ena socket gia thn syndesh
+                    System.out.println("Worker connected");
 
-            while (true) {
-                Socket socket = serverSocket.accept();//otan syndetai enas worker, to accept epistrefei ena socket gia thn syndesh
-                System.out.println("Worker connected");
+                    new Thread(new SRGWorkerHandler(socket)).start();//gia na dimiourghsei ena neo thread gia kathe worker pou syndetai
+                }
 
-                new Thread(new SRGWorkerHandler(socket)).start();//gia na dimiourghsei ena neo thread gia kathe worker pou syndetai
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-}
