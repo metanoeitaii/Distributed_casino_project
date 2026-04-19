864ebb14304e47c9b0ff5fcddf43907feff44ea4
ElenaKostara <elenakostara05@gmail.com>
2026-04-19 11:10:46 +0300

    Update RiskTables.java

--- src/main/java/common/RiskTables.java
+++ src/main/java/common/RiskTables.java
@@ -1,11 +1,10 @@
 package common;
 
-
 //pinakes pollaplasiastwn gia ka8e epipedo riskou 
+// index i = randomNumber % 10
+// jackpot an randomNumber % 100 == 0
 public class RiskTables{ 
 
-    //an randomNumber % 100 == 0 -> jackpot, oxi pinakas 
-    //alliws i = randomNumber % 10 -> multiplier apo pinaka, analoga RiskLevel 
     public static final double[] LOW = {0.0, 0.0, 0.0, 0.1, 0.5, 1.0, 1.1, 1.3, 2.0, 2.5};
     public static final double[] MEDIUM = {0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.5, 2.5, 3.5};
     public static final double[] HIGH = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 6.5};
@@ -20,7 +19,7 @@ public class RiskTables{
             case "high":
                 return HIGH;
             default:
-                return LOW; //gia na mhn krasarei an er8ei allh timh 
+                return LOW; //fallback gia agnwsth timh
         }
     }
 }
