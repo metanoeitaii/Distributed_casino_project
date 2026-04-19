e48eab89020a379137b12c24ff7fbf4352950567
ElenaKostara <elenakostara05@gmail.com>
2026-04-19 11:48:34 +0300

    Update MasterServer.java

--- src/main/java/master/MasterServer.java
+++ src/main/java/master/MasterServer.java
@@ -11,28 +11,20 @@
 
         
         public static void main(String[] args) {
-        try{                                                         
-            // to try to bazw epeidh petaei Exception sto object
-        ServerSocket serverSocket = new ServerSocket(clientport);
+        try (ServerSocket serverSocket = new ServerSocket(clientport)){                                                         
     
-        System.out.println("Perimenoume gia clients sto port 1000");
-        workerHosts.add("localhost");
-        workerPorts.add(1001);
-        System.out.println("Sundesh sto worker 1");
-        //loop gia na dexetai pollous clients
-        while (true) {
-            Socket sock2 =serverSocket.accept();
-            System.out.println("O client sindethike");
-            ClientHandler handler = new ClientHandler(sock2,workerHosts,workerPorts);
-            handler.start();
-                    
-                    
+            System.out.println("Perimenoume gia clients sto port 1000");
+            workerHosts.add("localhost");
+            workerPorts.add(1001);
+            System.out.println("Sundesh sto worker 1");
             
-
-            }
-            
-        
-        
+            //loop gia na dexetai pollous clients
+            while (true) {
+                Socket sock2 =serverSocket.accept();
+                System.out.println("O client sindethike");
+                ClientHandler handler = new ClientHandler(sock2,workerHosts,workerPorts);
+                handler.start();      
+            }    
     }catch(Exception e){
         System.out.println("Error");
         e.printStackTrace();
