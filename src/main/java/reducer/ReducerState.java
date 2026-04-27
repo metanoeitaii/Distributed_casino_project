092e580dddb87c7d4514c8e9735c97cb365ba4bc
ElenaKostara <elenakostara05@gmail.com>
2026-04-27 17:43:13 +0300

    Update ReducerState.java

--- src/main/java/reducer/ReducerState.java
+++ src/main/java/reducer/ReducerState.java
@@ -1,12 +1,15 @@
 package reducer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
 // koino state metajy twn threads tou reducer 
 public class ReducerState {
 
-    private Map<String, Double> totals;
+    private Map<String, Double> totals; // gia MAP 
+    private List<String[]> searchResults; // gia SEARCH - ka8e stoixeio einai array me ola ta pedia
     private int expectedWorkers;
     private int finishedWorkers;
 
@@ -14,14 +17,21 @@ public class ReducerState {
         this.expectedWorkers = expectedWorkers;
         this.finishedWorkers = 0;
         this.totals = new HashMap<>();
+        this.searchResults = new ArrayList<>();
     }
 
+    // gia MAP
     public synchronized void addPair(String key, double value) {//prosthetei value sto key h dimiourgei neo key an den yparxei
         if (totals.containsKey(key)) {
             totals.put(key, totals.get(key) + value);
         } else {
             totals.put(key, value);
         }
+    }
+
+    // gia SEARCH 
+    public synchronized void addSearchResult(String[] gameData){
+        searchResults.add(gameData);
     }
 
     // eidopoiei otan teleiwsoyn oloi oi workers 
@@ -47,4 +57,10 @@ public class ReducerState {
     public synchronized Map<String, Double> getTotalsCopy() {
         return new HashMap<>(totals);
     }
+
+    public synchronized List<String[]> getSearchResultsCopy() {
+        return new ArrayList<>(searchResults);
+    }
 }
+
+
