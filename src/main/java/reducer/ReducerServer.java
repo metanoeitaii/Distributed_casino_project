8c51bf4ddd8d59fc0f11c9badf5d1d42157c9ace
ElenaKostara <elenakostara05@gmail.com>
2026-04-27 17:42:51 +0300

    Update ReducerServer.java

--- src/main/java/reducer/ReducerServer.java
+++ src/main/java/reducer/ReducerServer.java
@@ -29,31 +29,40 @@ public class ReducerServer {
 
                 ObjectInputStream masterIn = new ObjectInputStream(masterSocket.getInputStream()); //diavazei apo ton master posoi workers 8a synde8oun
                 int expectedWorkers = (Integer) masterIn.readObject();
-                
+                String mode = (String) masterIn.readObject(); //MAP/SEARCH
+
                 ReducerState state = new ReducerState(expectedWorkers);//koino state pou krataei ta apotelesmata kai tis plirofories gia tous workers
 
                 for (int i = 0; i < expectedWorkers; i++) {
                     Socket workerSocket = serverSocket.accept();
                     System.out.println("Worker connected to reducer");
 
-                    Thread t = new Thread(new ReducerWorkerHandler(workerSocket, state)); //thread gia kathe worker pou syndeetai
+                    Thread t = new Thread(new ReducerWorkerHandler(workerSocket, state, mode)); //thread gia kathe worker pou syndeetai
                     t.start();
                 }
 
                 state.waitUntilAllWorkersFinish(); //mplokarei mexri na teleiwsoun oloi oi workers
 
-                // stelnei ta results ston master 
-                Map<String, Double> finalTotal = state.getTotalsCopy();
                 ObjectOutputStream masterOut  = new ObjectOutputStream(masterSocket.getOutputStream());
-                for (String key : finalTotal.keySet()) {
-                    masterOut.writeObject(key);
-                    masterOut.writeObject(finalTotal.get(key));
+                if (mode.equals("MAP")){
+                    //stelnei key + value
+                    Map<String, Double> finalTotal = state.getTotalsCopy();
+                    for (String key : finalTotal.keySet()) {
+                        masterOut.writeObject(key);
+                        masterOut.writeObject(finalTotal.get(key));
+                    } 
+                }else if (mode.equals("SEARCH")){
+                    //stelnei keys (gameName)
+                    for (String[] gameData : state.getSearchResultsCopy()){
+                        for (String field : gameData){
+                            masterOut.writeObject(field);
+                        }                 
+                    }
                 }
+
                 masterOut.writeObject(Message.END);
                 masterOut.flush();
-
                 System.out.println("Results sent to master");
-
                 masterSocket.close();
             }
 
