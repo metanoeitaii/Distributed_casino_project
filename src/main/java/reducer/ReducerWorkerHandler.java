e944ab9050aca8935eed6b2a8a8b46c9e7ae8b8c
ElenaKostara <elenakostara05@gmail.com>
2026-04-27 17:43:35 +0300

    Update ReducerWorkerHandler.java

--- src/main/java/reducer/ReducerWorkerHandler.java
+++ src/main/java/reducer/ReducerWorkerHandler.java
@@ -9,17 +9,17 @@ public class ReducerWorkerHandler implements Runnable {//
 
     private Socket socket;
     private ReducerState state;
+    private String mode; // MAP/SEARCH
 
-    public ReducerWorkerHandler(Socket socket, ReducerState state) {
+    public ReducerWorkerHandler(Socket socket, ReducerState state, String mode) {
         this.socket = socket;
         this.state = state;
+        this.mode = mode;
     }
 
     @Override
     public void run() {
-        try (
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        ) {
+        try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream());) {
             while (true) {
                 Object obj = in.readObject();
 
@@ -31,10 +31,29 @@ public class ReducerWorkerHandler implements Runnable {//
                     break;
                 }
 
-                String key = (String) obj;
-                Double value = (Double) in.readObject();
+                if (mode.equals("MAP")){
+                    String key = (String) obj;
+                    Double value = (Double) in.readObject();
+                    state.addPair(key, value);
+                }else if(mode.equals("SEARCH")){
+                    String gameName = (String) obj;
+                    String providerName = (String) in.readObject();
+                    String gameLogo = (String) in.readObject();
+                    double stars = (double) in.readObject();
+                    int noOfVotes = (int) in.readObject();
+                    double minBet = (double) in.readObject();
+                    double maxBet = (double) in.readObject();
+                    String riskLevel = (String) in.readObject();
+                    String betCategory = (String) in.readObject();
+                    double jackpot = (double) in.readObject();
 
-                state.addPair(key, value);
+                    String[] gameData = {gameName, providerName, gameLogo,
+                        String.valueOf(stars), String.valueOf(noOfVotes),
+                        String.valueOf(minBet), String.valueOf(maxBet),
+                        riskLevel, betCategory, String.valueOf(jackpot)};
+
+                    state.addSearchResult(gameData);
+                }
             }
 
         } catch (Exception e) {
